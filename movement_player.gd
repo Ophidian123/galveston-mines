d32464c5c04029f57347956c487a0193f3bf6685
ophidian <nemanipavan12@gmail.com>
2026-09-17 21:03:57 -0500

    trying somehting

--- movement_player.gd
+++ movement_player.gd
@@ -19,6 +19,13 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
+	
+	if Input.is_action_pressed("sprint") and is_on_floor():
+		current_SPEED = sprint_SPEED
+	else:
+		current_SPEED = walk_SPEED
+		
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
