9f48d81504ae7aae62313e72c856804c83ec88c3
karstenk42 <karkoc42@gmail.com>
2026-09-19 16:25:11 -0500

    Commit

--- KK/Scripts/player.gd
+++ KK/Scripts/player.gd
@@ -22,10 +22,16 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * current_SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_SPEED)
 
+
+
 	move_and_slide()
+
+
+func _on_killzone_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
