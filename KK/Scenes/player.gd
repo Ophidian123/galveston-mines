290a270b12b3c3dbbdfa065c361b22ee078abf81
karstenk42 <karkoc42@gmail.com>
2026-09-20 13:28:59 -0500

    Slime Enemy and Background

--- KK/Scenes/player.gd
+++ KK/Scenes/player.gd
@@ -16,11 +16,11 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$sprite2d.flip_h = (direction<0)
+ 
 
 	move_and_slide()
