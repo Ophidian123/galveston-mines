090a5d7811ad1295c12cb6a3ddc824a85a9adbc7
karstenk42 <karkoc42@gmail.com>
2026-09-20 16:44:16 -0500

    Linking Main Menu buttons to functions

--- KK/Scripts/main_menu.gd
+++ KK/Scripts/main_menu.gd
@@ -12,12 +12,12 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
 
 
 func _on_settings_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
 
 
 func _on_help_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/help.tscn")
