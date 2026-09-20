80414841626c42054c3eb69a837966a6ed3f62ea
karstenk42 <karkoc42@gmail.com>
2026-09-20 18:44:56 -0500

    coin pickup sounds

--- KK/Scripts/coin.gd
+++ KK/Scripts/coin.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 @onready var game_manager: Node = %GameManager
  
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("Pickup_sound")
