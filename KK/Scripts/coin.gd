e6ffb0d9e097e7d2105895ef7f64f3cc6500baf8
karstenk42 <karkoc42@gmail.com>
2026-09-20 18:01:17 -0500

    Buttons, Score, etc.

--- KK/Scripts/coin.gd
+++ KK/Scripts/coin.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 
+@onready var game_manager: Node = %GameManager
+ 
 
 
 func _on_body_entered(body: Node2D) -> void:
-	print('+1 Coin!') # Replace with function body.
+	game_manager.add_point()
 	queue_free()
